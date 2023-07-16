c587252c0bd6fc3f347e3d8fe19a39619b69cdd2
Anurag Jha <toanuragjha@gmail.com>
2023-07-16 00:35:41 -0700

    feat-agents: stability upscale

--- autobots/conn/stability/stability_data.py
+++ autobots/conn/stability/stability_data.py
@@ -1,26 +1,35 @@
 from typing import Any, Sequence
-
 from pydantic import BaseModel
 import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
 
 
 class StabilityReq(BaseModel):
+    # https://platform.stability.ai/docs/features/api-parameters
     prompt: str | list[str] | list
-    # init_image: Image | None = None
-    # mask_image: Image | None = None
+    init_image: Any | None = None
+    mask_image: Any | None = None
     height: int = 512
     width: int = 512
-    start_schedule: float = 1.0
-    end_schedule: float = 0.01
+    start_schedule: float = 0.6
+    end_schedule: float = 0.1
     cfg_scale: float = 7.0
-    sampler: Any = None
-    steps: int | None = 30
+    sampler: Any = generation.SAMPLER_K_DPMPP_2M
+    steps: int | None = 50
     seed: Sequence[int] | int = 0
     samples: int = 1
     safety: bool = True
     classifiers: Any | None = None
-    guidance_preset: int = generation.GUIDANCE_PRESET_NONE
+    guidance_preset: int = generation.GUIDANCE_PRESET_FAST_GREEN
     guidance_cuts: int = 0
     guidance_strength: float | None = None
     guidance_prompt: str | None = None
     guidance_models: list[str] | None = None
+
+
+class StabilityUpscaleReq(BaseModel):
+    init_image: Any
+    width: int = None
+    # prompt: str = None
+    # seed: int = None
+    # steps: int = 30
+    # cfg_scale: int = 7
