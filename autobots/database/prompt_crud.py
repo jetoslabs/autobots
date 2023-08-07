0c198a885bb7cedb7cdea17d04504b4aaaf1dd2f
Anurag Jha <toanuragjha@gmail.com>
2023-08-07 01:12:41 -0700

    feat-agents: user_prompts apis

--- autobots/database/prompt_crud.py
+++ autobots/database/prompt_crud.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from fastapi import Depends
 from sqlalchemy import UUID
 from sqlalchemy.orm import Session
 
@@ -10,47 +11,49 @@ from autobots.database.database_models import PromptORM
 class PromptCRUD:
 
     @staticmethod
-    async def create(prompt: PromptORM, db: Session = next(get_db())) -> PromptORM:
+    async def create(prompt: PromptORM, db: Session = Depends(get_db)) -> PromptORM:
         db.add(prompt)
         db.commit()
         db.refresh(prompt)
         return prompt
 
     @staticmethod
-    async def read(user_id: UUID, id: UUID, db: Session = get_db()) -> List[PromptORM]:
+    async def list(
+            user_id: UUID, limit: int = 100, offset: int = 0, db: Session = Depends(get_db)
+    ) -> List[PromptORM]:
+        if limit < 0 or limit > 100:
+            limit = 100
+        if offset < 0:
+            offset = 0
         prompts = db.query(PromptORM) \
             .filter_by(user_id=user_id) \
-            .filter_by(id=id) \
+            .limit(limit) \
+            .offset(offset * limit) \
             .all()
         return prompts
-        # session: SessionLocal = SessionLocal()
-        # try:
-        #     prompts = session.query(PromptORM) \
-        #         .filter_by(user_id=user_id) \
-        #         .filter_by(id=id) \
-        #         .all()
-        #     return prompts
-        # finally:
-        #     session.close()
 
     @staticmethod
-    async def delete(prompt: PromptORM, db: Session = get_db()):
+    async def read(user_id: UUID, id: UUID, db: Session = Depends(get_db)) -> PromptORM:
+        prompt = db.query(PromptORM) \
+            .filter_by(user_id=user_id) \
+            .filter_by(id=id) \
+            .first()
+        return prompt
+
+    @staticmethod
+    async def delete(prompt: PromptORM, db: Session = Depends(get_db)) -> PromptORM:
         db.delete(prompt)
-        # session: SessionLocal = SessionLocal()
-        # try:
-        #     session.delete(prompt)
-        #     session.commit()
-        # finally:
-        #     session.close()
+        db.commit()
+        return prompt
 
     @staticmethod
     async def upsert(
             del_prompt: PromptORM,
             new_prompt: PromptORM,
-            db: Session = get_db()
-    ):
+            db: Session = Depends(get_db)
+    ) -> PromptORM:
         await PromptCRUD.delete(del_prompt, db)
-        await PromptCRUD.create(new_prompt, db)
+        return await PromptCRUD.create(new_prompt, db)
 
     @staticmethod
     async def read_by_name(user_id: UUID, name: str, db: Session = get_db()) -> List[PromptORM]:
@@ -59,12 +62,3 @@ class PromptCRUD:
             .filter_by(name=name) \
             .all()
         return prompts
-        # session: SessionLocal = SessionLocal()
-        # try:
-        #     prompts = session.query(PromptORM) \
-        #         .filter_by(user_id=user_id) \
-        #         .filter_by(name=name) \
-        #         .all()
-        #     return prompts
-        # finally:
-        #     session.close()
