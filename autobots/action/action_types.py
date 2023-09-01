39ee006627e891652e7d2d0a62a75749adc5aad3
Anurag Jha <toanuragjha@gmail.com>
2023-09-01 10:23:23 -0700

    feat-mvp: api_actions

--- autobots/action/action_types.py
+++ autobots/action/action_types.py
@@ -6,6 +6,5 @@ class ActionType(str, Enum):
 
     gen_image_dalle_openai = "gen_image_dalle_openai"
     gen_image_stability_ai = "gen_image_stability_ai"
-    # gen_text: GenText
-    # gen_image: GenImage
-    # gen_video: GenVideo
+
+
