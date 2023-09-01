d927084394b0244a833adb85ac49d26ef64607e1
Anurag Jha <toanuragjha@gmail.com>
2023-09-01 09:14:00 -0700

    feat-mvp: prompt graph to_del

--- tests/graphs/test_user_graphs.py
+++ tests/graphs/test_user_graphs.py
@@ -9,8 +9,8 @@ from autobots.conn.openai.chat import ChatReq, Message, Role
 from autobots.core.settings import get_settings
 from autobots.core.utils import gen_random_str
 from autobots.database.base import get_db
-from autobots.graphs.graph_orm_model import GraphORM
-from autobots.graphs.user_graphs import UserGraphCreateInput, UserGraphs
+from autobots.graphs.to_del.graph_orm_model import GraphORM
+from autobots.graphs.to_del.user_graphs import UserGraphCreateInput, UserGraphs
 from autobots.prompts.prompt_orm_model import PromptORM
 from autobots.prompts.target_platform import LLMTargetPlatform
 from autobots.prompts.user_prompts import UserPrompts, UserPromptCreateInput, Input
