44c6edee7cb21258b8ff5b77d33b897db753581d
Anurag Jha <toanuragjha@gmail.com>
2023-08-31 23:32:25 -0700

    feat-mvp: user_action_graph test

--- tests/graphs/test_user_action_graph.py
+++ tests/graphs/test_user_action_graph.py
@@ -12,6 +12,8 @@ from autobots.core.settings import get_settings
 from autobots.core.utils import gen_random_str
 from autobots.database.mongo_base import get_mongo_db
 from autobots.graphs.action_graph import ActionGraph
+from autobots.graphs.action_graph_doc_model import ActionGraphCreate
+from autobots.graphs.user_action_graph import UserActionGraphs
 from autobots.prompts.user_prompts import Input
 from autobots.user.user_orm_model import UserORM
 
@@ -30,13 +32,17 @@ async def test_user_graph_run_happy_path(set_settings):
 
     db = next(get_mongo_db())
 
-    # create actions
     user_actions = UserActions(user=user)
+    user_action_graph = UserActionGraphs(user=user)
+
+    # create actions
     action_llm_persona = await create_action_persona(user_actions, db, rand)
     action_llm_manager = await create_action_manager(user_actions, db, rand)
     action_llm_product = await create_action_product(user_actions, db, rand)
     action_llm_creative = await create_action_creative(user_actions, db, rand)
     action_llm_jingle = await create_action_jingle(user_actions, db, rand)
+    # init action graph
+    action_graph_doc = None
 
     try:
         # create action graph
@@ -46,23 +52,34 @@ async def test_user_graph_run_happy_path(set_settings):
             str(action_llm_product.id): [str(action_llm_creative.id), str(action_llm_jingle.id)]
         }
 
-        # run action graph
+        # run action graph on fly
         user_input = Input(input="Campaign for Nike shoes during Diwali Festival")
-        action_response = await ActionGraph.run(user, user_input, action_graph, db)
+        action_graph_response = await ActionGraph.run(user, user_input, action_graph, db)
+        assert len(action_graph_response) > 1
 
-        # validate
-        assert len(action_response) > 1
+        # save action graph
+        action_graph_create = ActionGraphCreate(
+            name="Marketing Dept", graph=action_graph
+        )
+        action_graph_doc = await user_action_graph.create_action_graph(action_graph_create, db)
+
+        # run saved action graph
+        input = Input(input="Create ad for sports shoes")
+        action_graph_resp = await user_action_graph.run_action_graph(action_graph_doc.id, input, db)
+        assert len(action_graph_resp) > 1
 
     except Exception as e:
         assert e is not None
 
     finally:
-        # cleanup
+        # cleanup actions
         await user_actions.delete_action(action_llm_manager.id, db)
         await user_actions.delete_action(action_llm_persona.id, db)
         await user_actions.delete_action(action_llm_product.id, db)
         await user_actions.delete_action(action_llm_creative.id, db)
         await user_actions.delete_action(action_llm_jingle.id, db)
+        # cleanup action graph
+        await user_action_graph.delete_action_graph(action_graph_doc.id, db)
 
 
 @pytest.mark.asyncio
