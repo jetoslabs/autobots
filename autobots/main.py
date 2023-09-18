f4a05e710858b99c95417ef010710c6a00326b1f
Anurag Jha <toanuragjha@gmail.com>
2023-09-17 23:25:01 -0700

    feat-api-usage: allow_origins localhost:3000

--- autobots/main.py
+++ autobots/main.py
@@ -17,7 +17,7 @@ app = FastAPI(lifespan=lifespan, **FastAPIDesc().model_dump())
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
