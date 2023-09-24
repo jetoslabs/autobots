0f183e3f2c029bac66fdabd42f7b12673b8b791a
Anurag Jha <toanuragjha@gmail.com>
2023-09-24 00:16:41 -0700

    feat-agents: Vector RAG action

---
+++ autobots/action/IActionVectorSearch.py
@@ -0,0 +1,26 @@
+from abc import abstractmethod
+from typing import Any
+
+from autobots.action.IAction import IAction
+from autobots.conn.openai.chat import Message
+from autobots.prompts.user_prompts import Input
+
+
+class IActionVectorSearch(IAction):
+
+    @abstractmethod
+    def __init__(self, action_data: Any):
+        self.action_data = action_data
+
+    @abstractmethod
+    async def run_action(self, action_input: Input) -> Message:
+        pass
+
+    @abstractmethod
+    async def invoke_action(self, input_str: str) -> Message:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    async def instruction() -> str:
+        pass
