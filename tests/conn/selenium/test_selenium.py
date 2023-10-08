68b3b4f05958d3d58bcaf3348a93779aec2ea55e
Anurag Jha <toanuragjha@gmail.com>
2023-10-08 16:45:45 -0700

    feat-unstructured: re-enabled selenium

--- tests/conn/selenium/test_selenium.py
+++ tests/conn/selenium/test_selenium.py
@@ -12,10 +12,14 @@ async def set_settings():
     settings = get_settings(_env_file='.env.local')
 
 
-@pytest.mark.skip(reason="Selenium driver not working")
+# @pytest.mark.skip(reason="Selenium driver not working")
+@pytest.mark.asyncio
+async def test_read_url_text_happy_path(set_settings):
+    url_text = await get_selenium().read_url_text(HttpUrl("https://google.com"))
+    assert "Gmail" in url_text
+
+
 @pytest.mark.asyncio
 async def test_read_url_happy_path(set_settings):
-
-    url_text = await get_selenium().read_url_text(HttpUrl("https://meetkiwi.co"))
-    assert "Meetkiwi" in url_text
-
+    html = await get_selenium().read_url(HttpUrl("https://google.com"))
+    assert "Google" in html
