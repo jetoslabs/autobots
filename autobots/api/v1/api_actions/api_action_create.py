5dcc41636d86f28a65c8c67dbe3e5fa6bebde92e
Anurag Jha <toanuragjha@gmail.com>
2023-10-18 16:28:29 -0700

    feat-stable-diffusion: chore

--- autobots/api/v1/api_actions/api_action_create.py
+++ autobots/api/v1/api_actions/api_action_create.py
@@ -7,7 +7,8 @@ from pymongo.database import Database
 from autobots.action.action_doc_model import ActionDoc, ActionCreate
 from autobots.action.action_type.action_text2img.action_gen_image_dalle_openai_v2 import ActionCreateGenImageDalleOpenai
 from autobots.action.action_type.action_text2img.action_gen_image_stability_ai_v2 import ActionCreateGenImageStabilityAi
-from autobots.action.action_type.action_text2text.action_gen_text_llm_chat_openai_v2 import ActionCreateGenTextLlmChatOpenai
+from autobots.action.action_type.action_text2text.action_gen_text_llm_chat_openai_v2 import \
+    ActionCreateGenTextLlmChatOpenai
 from autobots.action.action_type.action_text2text.action_gen_text_llm_chat_with_vector_search_openai import \
     ActionCreateGenTextLlmChatWithVectorSearchOpenai
 from autobots.action.user_actions import UserActions
