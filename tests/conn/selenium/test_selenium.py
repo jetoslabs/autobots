9c4ce4acdd90de924f3d1f1aa3f936fb8efce710
Anurag Jha <toanuragjha@gmail.com>
2023-10-21 22:21:25 -0700

    feat-chore: selenium chore

--- tests/conn/selenium/test_selenium.py
+++ tests/conn/selenium/test_selenium.py
@@ -15,6 +15,6 @@ async def test_read_url_text_happy_path(set_test_settings):
 # TODO: Make Selenium work, currently unable to install chromedriver in image
 # @pytest.mark.skip(reason="Selenium driver not working")
 @pytest.mark.asyncio
-async def test_read_url_happy_path(set_settings):
+async def test_read_url_happy_path(set_test_settings):
     html = await get_selenium().read_url(HttpUrl("https://google.com"))
     assert "Google" in html
