725067a2de658062301b1f44abe8162afa62821a
Anurag Jha <toanuragjha@gmail.com>
2023-11-03 16:38:37 -0700

    feat-chore: action_graph chore

--- autobots/action_graph/action_graph/action_graph.py
+++ autobots/action_graph/action_graph/action_graph.py
@@ -11,6 +11,28 @@ from autobots.user.user_orm_model import UserORM
 class ActionGraph:
 
     @staticmethod
+    async def run(user: UserORM, input: TextObj, node_action_map: Dict[str, str], graph_map: Dict[str, List[str]], db: Database) -> Dict[str, Any]:
+        total_nodes = await ActionGraph.get_nodes(graph_map)
+        inverted_map = await ActionGraph.invert_map(graph_map)
+        action_response: Dict[str, Any] = {}
+
+        user_actions = UserActions(user, db)
+        while len(action_response) != len(total_nodes):
+            for node, values in inverted_map.items():
+                if await ActionGraph.is_work_done([node], action_response) or \
+                        not await ActionGraph.is_work_done(values, action_response):
+                    continue
+                if len(values) == 0:
+                    action_result = await user_actions.run_action(node_action_map.get(node), input.model_dump())
+                    action_response[node] = action_result
+                else:
+                    action_input = await ActionGraph.to_input(values, action_response)
+                    action_result = await user_actions.run_action(node_action_map.get(node), action_input.model_dump())
+                    action_response[node] = action_result
+
+        return action_response
+
+    @staticmethod
     async def get_nodes(graph: Dict[str, List[str]]) -> Set[str]:
         nodes: Set[str] = set()
         for key, values in graph.items():
@@ -32,28 +54,6 @@ class ActionGraph:
                 inverted_map[value] = inverted_map.get(value) + [key]
 
         return inverted_map
-
-    @staticmethod
-    async def run(user: UserORM, input: TextObj, node_action_map: Dict[str, str], graph_map: Dict[str, List[str]], db: Database) -> Dict[str, Any]:
-        total_nodes = await ActionGraph.get_nodes(graph_map)
-        inverted_map = await ActionGraph.invert_map(graph_map)
-        action_response: Dict[str, Any] = {}
-
-        user_actions = UserActions(user, db)
-        while len(action_response) != len(total_nodes):
-            for node, values in inverted_map.items():
-                if await ActionGraph.is_work_done([node], action_response) or \
-                        not await ActionGraph.is_work_done(values, action_response):
-                    continue
-                if len(values) == 0:
-                    action_result = await user_actions.run_action(node_action_map.get(node), input)
-                    action_response[node] = action_result
-                else:
-                    action_input = await ActionGraph.to_input(values, action_response)
-                    action_result = await user_actions.run_action(node_action_map.get(node), action_input, db)
-                    action_response[node] = action_result
-
-        return action_response
 
     @staticmethod
     async def is_work_done(values: List[str], action_response: Dict[str, Any]) -> bool:
