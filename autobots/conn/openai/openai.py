1d2b3a306a5138fdef2e714df32d741e106d9471
Anurag Jha <toanuragjha@gmail.com>
2023-11-07 23:15:56 -0800

    feat-chore: updated conn openai chat

--- autobots/conn/openai/openai.py
+++ autobots/conn/openai/openai.py
@@ -2,10 +2,11 @@ import time
 from functools import lru_cache
 from typing import List
 
-import openai
-from openai.openai_object import OpenAIObject
+from openai import AsyncOpenAI, AsyncStream
+from openai.types import CreateEmbeddingResponse, ImagesResponse
+from openai.types.chat import ChatCompletion, ChatCompletionChunk
 
-from autobots.conn.openai.chat import ChatReq, ChatRes
+from autobots.conn.openai.chat import ChatReq
 from autobots.conn.openai.embedding import EmbeddingReq, EmbeddingRes
 from autobots.conn.openai.image_model import ImageReq, ImageRes
 from autobots.core.log import log
@@ -15,28 +16,38 @@ from autobots.core.settings import Settings, SettingsProvider
 class OpenAI:
 
     def __init__(self, org_id: str, api_key: str):
-        # once set, any invocation of openai has state
-        # openai here is global
-        openai.organization = org_id
-        openai.api_key = api_key
+        self.client = AsyncOpenAI(
+            api_key=api_key,
+            organization=org_id,
+            timeout=180
+        )
 
-    async def chat(self, chat_req: ChatReq) -> ChatRes | None:
+    async def chat(self, chat_req: ChatReq) -> ChatCompletion | AsyncStream[ChatCompletionChunk] | None:
         max_retry = 3
         for i in range(max_retry):
             try:
                 log.trace("Starting OpenAI Chat, try: 1")
-                res: OpenAIObject = await openai.ChatCompletion.acreate(**chat_req.model_dump(), timeout=180)
+                res: ChatCompletion = await self.client.chat.completions.create(**chat_req.model_dump(), timeout=180)
                 log.trace("Completed OpenAI Chat")
-                resp: ChatRes = ChatRes.model_validate(res)
-                return resp
+                if isinstance(res, AsyncStream):
+                    return self.yield_chat_chunks(res)
+                else:
+                    return res
             except Exception as e:
                 log.exception(e)
                 time.sleep(5)
 
+    async def yield_chat_chunks(self, chat_res: AsyncStream[ChatCompletionChunk]) -> ChatCompletionChunk | None:
+        try:
+            async for part in chat_res:
+                yield part
+        except Exception as e:
+            log.exception(e)
+
     async def embedding(self, embedding_req: EmbeddingReq) -> EmbeddingRes | None:
         try:
             log.trace("Starting OpenAI Embedding")
-            res: OpenAIObject = await openai.Embedding.acreate(**embedding_req.model_dump())
+            res: CreateEmbeddingResponse = await self.client.embeddings.create(**embedding_req.model_dump())
             log.trace("Completed OpenAI Embedding")
             resp: EmbeddingRes = EmbeddingRes(**res.to_dict())
             return resp
@@ -46,7 +57,7 @@ class OpenAI:
     async def create_image(self, image_req: ImageReq) -> List[ImageRes]:
         try:
             log.trace("Starting OpenAI create image")
-            res: OpenAIObject = await openai.Image.acreate(**image_req.model_dump())
+            res: ImagesResponse = await self.client.images.generate(**image_req.model_dump())
             log.trace("Completed OpenAI create image")
             images = []
             for data in res.data:
--- tests/conn/openai/test_chat.py
+++ tests/conn/openai/test_chat.py
@@ -1,15 +1,36 @@
 import pytest as pytest
+from openai import AsyncStream
+from openai.types.chat import ChatCompletion, ChatCompletionUserMessageParam, ChatCompletionSystemMessageParam, \
+    ChatCompletionChunk
 
-from autobots.conn.openai.chat import ChatReq, Message, ChatRes
+from autobots.conn.openai.chat import ChatReq
 from autobots.conn.openai.openai import get_openai
+
 
 @pytest.mark.asyncio
 async def test_chat_happy_path(set_test_settings):
-    msg0 = Message(role="system", content="You are a helpful assistant.")
-    msg1 = Message(role="user", content="Most famous Mechanics law")
-    params = ChatReq(messages=[msg0, msg1])
+    msg0 = ChatCompletionSystemMessageParam(role="system", content="You are a helpful assistant.")
+    msg1 = ChatCompletionUserMessageParam(role="user", content="Most famous Mechanics law")
+    params = ChatReq(messages=[msg0, msg1], model="gpt-3.5-turbo-16k-0613")
 
-    resp: ChatRes = await get_openai().chat(chat_req=params)
+    resp: ChatCompletion = await get_openai().chat(chat_req=params)
 
     assert "assistant" == resp.choices[0].message.role
     assert "Newton" in resp.choices[0].message.content
+
+
+@pytest.mark.asyncio
+async def test_chat_stream_happy_path(set_test_settings):
+    msg0 = ChatCompletionSystemMessageParam(role="system", content="You are a helpful assistant.")
+    msg1 = ChatCompletionUserMessageParam(role="user", content="Most famous Mechanics law")
+    params = ChatReq(messages=[msg0, msg1], model="gpt-3.5-turbo-16k-0613", stream=True)
+
+    stream = await get_openai().chat(chat_req=params)
+
+    collected_words = ""
+    async for part in stream:
+        assert part.object == "chat.completion.chunk"
+        if part.choices[0].delta.content:
+            collected_words = collected_words + part.choices[0].delta.content.lower()
+
+    assert "newton" in collected_words
