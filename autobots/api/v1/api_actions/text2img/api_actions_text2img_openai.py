17b84b15cff99f1d4ddd29a9f43fe9f362bf5516
Anurag Jha <toanuragjha@gmail.com>
2023-11-09 17:10:18 -0800

    feat-chore: run api for dall-e

--- autobots/api/v1/api_actions/text2img/api_actions_text2img_openai.py
+++ autobots/api/v1/api_actions/text2img/api_actions_text2img_openai.py
@@ -2,12 +2,16 @@ from uuid import UUID
 
 import gotrue
 from fastapi import APIRouter, Depends, HTTPException
+from openai.types import ImagesResponse
 from pymongo.database import Database
 
 from autobots.action.action.action_doc_model import ActionDoc, ActionCreate
+from autobots.action.action.common_action_models import TextObj
 from autobots.action.action.user_actions import UserActions
-from autobots.action.action_type.action_text2img.action_text2img_dalle_openai_v2 import ActionCreateGenImageDalleOpenai
+from autobots.action.action_type.action_text2img.action_text2img_dalle_openai_v2 import ActionCreateGenImageDalleOpenai, \
+    ActionGenImageDalleOpenAiV2
 from autobots.auth.security import get_user_from_access_token
+from autobots.conn.openai.image_model import ImageReq
 from autobots.core.database.mongo_base import get_mongo_db
 from autobots.core.log import log
 from autobots.user.user_orm_model import UserORM
@@ -16,7 +20,7 @@ router = APIRouter()
 
 
 @router.post("/text2img/dalle/openai")
-async def create_action_gen_image_dalle_openai(
+async def create_text2img_dalle_openai(
         action_create: ActionCreateGenImageDalleOpenai,
         user_res: gotrue.UserResponse = Depends(get_user_from_access_token),
         db: Database = Depends(get_mongo_db)
@@ -27,6 +31,26 @@ async def create_action_gen_image_dalle_openai(
             ActionCreate(**action_create.model_dump())
         )
         return action_doc
+    except Exception as e:
+        log.exception(e)
+        raise HTTPException(500)
+
+
+@router.post("/text2img/dalle/openai/{action_id}/run")
+async def run_text2img_dalle_openai(
+        action_id: str,
+        action_input: TextObj,
+        user_res: gotrue.UserResponse = Depends(get_user_from_access_token),
+        db: Database = Depends(get_mongo_db)
+) -> ImagesResponse:
+    try:
+        user_orm = UserORM(id=UUID(user_res.user.id))
+        action = await UserActions(user=user_orm, db=db).get_action(action_id)
+        text2img = ActionGenImageDalleOpenAiV2(
+            ImageReq.model_validate(action.config)
+        )
+        resp = await text2img.run_action(action_input)
+        return resp
     except Exception as e:
         log.exception(e)
         raise HTTPException(500)
