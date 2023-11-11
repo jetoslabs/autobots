e0362fc73d3a7464b66516a5210b249ab7dc1c40
Anurag Jha <toanuragjha@gmail.com>
2023-11-11 01:31:30 -0800

    feat-chore: audio2text_translation_openai action api

---
+++ autobots/action/action_type/action_audio2text/action_audio2text_translation_openai.py
@@ -0,0 +1,47 @@
+from typing import Type
+
+from openai.types.audio import Translation
+from pydantic import BaseModel, HttpUrl, ValidationError
+
+from autobots.action.action_type.abc.IAction import IAction, ActionConfigType, ActionInputType, ActionOutputType
+from autobots.action.action_type.action_types import ActionType
+from autobots.conn.openai.openai_client import get_openai
+from autobots.conn.openai.translation_model import TranslationReq
+from autobots.core.log import log
+
+
+class AudioRes(BaseModel):
+    url: str
+
+
+class ActionAudio2TextTranslationOpenai(IAction[TranslationReq, AudioRes, Translation]):
+    type = ActionType.audio2text_translation_openai
+
+    @staticmethod
+    def get_config_type() -> Type[ActionConfigType]:
+        return TranslationReq
+
+    @staticmethod
+    def get_input_type() -> Type[ActionInputType]:
+        return AudioRes
+
+    @staticmethod
+    def get_output_type() -> Type[ActionOutputType]:
+        return Translation
+
+    def __init__(self, action_config: TranslationReq):
+        super().__init__(action_config)
+
+    async def run_action(self, action_input: AudioRes) -> Translation | None:
+        try:
+            if self.action_config.file_url is None and action_input.url is None:
+                return None
+            if action_input.url is not None:
+                self.action_config.file_url = HttpUrl(action_input.url)
+
+            translation = await get_openai().translation(self.action_config)
+            return translation
+        except ValidationError as e:
+            log.exception(e)
+        except Exception as e:
+            log.exception(e)
