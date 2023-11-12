66885dab3f358f82912aff4114c1a5aa596c86fd
Anurag Jha <toanuragjha@gmail.com>
2023-11-12 12:23:33 -0800

    feat-chore: openai_embeddings package to manage code

--- autobots/conn/openai/openai_client.py
+++ autobots/conn/openai/openai_client.py
@@ -1,12 +1,12 @@
 from functools import lru_cache
 
 from openai import AsyncOpenAI
-from openai.types import CreateEmbeddingResponse, ImagesResponse
+from openai.types import ImagesResponse
 
-from autobots.conn.openai.embedding import EmbeddingReq, EmbeddingRes
 from autobots.conn.openai.image_model import ImageReq
 from autobots.conn.openai.openai_audio.openai_audio import OpenaiAudio
 from autobots.conn.openai.openai_chat.openai_chat import OpenaiChat
+from autobots.conn.openai.openai_embeddings.openai_embeddings import OpenaiEmbeddings
 from autobots.core.log import log
 from autobots.core.settings import Settings, SettingsProvider
 
@@ -17,16 +17,8 @@ class OpenAI:
         self.client = AsyncOpenAI(api_key=api_key,organization=org_id,timeout=180)
         self.openai_audio = OpenaiAudio(self.client)
         self.openai_chat = OpenaiChat(self.client)
+        self.openai_embeddings = OpenaiEmbeddings(self.client)
 
-    async def embedding(self, embedding_req: EmbeddingReq) -> EmbeddingRes | None:
-        try:
-            log.trace("Starting OpenAI Embedding")
-            res: CreateEmbeddingResponse = await self.client.embeddings.create(**embedding_req.model_dump())
-            log.trace("Completed OpenAI Embedding")
-            resp: EmbeddingRes = EmbeddingRes(**res.model_dump())
-            return resp
-        except Exception as e:
-            log.exception(e)
 
     async def create_image(self, image_req: ImageReq) -> ImagesResponse | None:
         try:
