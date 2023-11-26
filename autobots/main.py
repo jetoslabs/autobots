80a884e420fb9eadf372675a6e17f3578c61eaf8
Anurag Jha <toanuragjha@gmail.com>
2023-11-25 23:33:26 -0400

    feat-chore: main chore

--- autobots/main.py
+++ autobots/main.py
@@ -1,8 +1,10 @@
+import uvicorn
 from ddtrace import patch
 from fastapi import FastAPI
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.middleware.cors import CORSMiddleware
 
+from autobots import SettingsProvider
 from autobots.api.ui import ui
 from autobots.api.v1 import v1
 from autobots.core.fastapi_desc import FastAPIDesc
@@ -26,15 +28,15 @@ app.include_router(v1.router)
 app.include_router(router=v1.router_docs)
 app.include_router(router=ui.router)
 
-# if __name__ == "__main__":
-#     SettingsProvider.set()
-#     settings = SettingsProvider.sget()
-#     # Run server
-#     uvicorn.run(
-#         "main:app",
-#         host=settings.APP_HOST,
-#         port=settings.APP_PORT,
-#         reload=settings.APP_RELOAD,
-#         log_level=settings.APP_LOG_LEVEL,
-#         workers=settings.APP_WORKERS
-#     )
+if __name__ == "__main__":
+    SettingsProvider.set()
+    settings = SettingsProvider.sget()
+    # Run server
+    uvicorn.run(
+        "main:app",
+        host="localhost",
+        port=8000,
+        reload=True,
+        log_level="debug",
+        workers=3,
+    )
