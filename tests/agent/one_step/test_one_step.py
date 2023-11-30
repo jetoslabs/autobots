13ca1b6cf530f7d24b0a5d8f284e86000555be3b
Anurag Jha <toanuragjha@gmail.com>
2023-11-29 18:46:09 -0800

    feat-chore: commenting agents tests

--- tests/agent/one_step/test_one_step.py
+++ tests/agent/one_step/test_one_step.py
@@ -4,7 +4,7 @@ from autobots.agent.one_step import AgentData, OneStepAgent
 
 
 # TODO: Make Selenium work, currently unable to install chromedriver in image
-# @pytest.mark.skip(reason="One step agent stalling, selenium tests should pass")
+@pytest.mark.skip(reason="One step agent stalling")
 @pytest.mark.asyncio
 async def test_one_step_happy_path_1(set_test_settings):
     agent_data = AgentData(goal="Act as an effective google search advert maker. Create advert for Nike shoes")
