3783903f82a4a405c09739bdced5c14e4ae40bb8
Anurag Jha <toanuragjha@gmail.com>
2023-12-04 06:20:59 -0800

    feat-openai-assistant: datastore_meta in mongo

--- autobots/datastore/datastore_meta_orm_model.py
+++ autobots/datastore/datastore_meta_orm_model.py
@@ -1,28 +1,28 @@
-import uuid
-from datetime import datetime
-
-from pydantic import BaseModel
-from sqlalchemy import ForeignKey, Column, UUID, String, DateTime, func
-
-from autobots.core.database.base import Base
-
-
-class DatastoreMetaModel(BaseModel):
-    id: str
-    name: str
-    user_id: uuid.UUID
-    created_at: datetime
-
-
-class DatastoreMetaORM(Base):
-    __tablename__ = "datastore_meta"
-
-    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id'), primary_key=True)
-    name = Column(String, primary_key=True)
-    id = Column(String)
-    created_at = Column(DateTime, server_default=func.now())
-
-    def __init__(self, user_id: UUID, name: str, id: str):
-        self.user_id = user_id
-        self.name = name
-        self.id = id
+# import uuid
+# from datetime import datetime
+#
+# from pydantic import BaseModel
+# from sqlalchemy import ForeignKey, Column, UUID, String, DateTime, func
+#
+# from autobots.core.database.base import Base
+#
+#
+# class DatastoreMetaModel(BaseModel):
+#     id: str
+#     name: str
+#     user_id: uuid.UUID
+#     created_at: datetime
+#
+#
+# class DatastoreMetaORM(Base):
+#     __tablename__ = "datastore_meta"
+#
+#     user_id = Column(UUID(as_uuid=True), ForeignKey('users.id'), primary_key=True)
+#     name = Column(String, primary_key=True)
+#     id = Column(String)
+#     created_at = Column(DateTime, server_default=func.now())
+#
+#     def __init__(self, user_id: UUID, name: str, id: str):
+#         self.user_id = user_id
+#         self.name = name
+#         self.id = id
--- autobots/datastore/user_datastore.py
+++ autobots/datastore/user_datastore.py
@@ -1,50 +1,64 @@
 from typing import List
 
-from sqlalchemy.orm import Session
+from fastapi import HTTPException
+from pymongo.database import Database
+from pymongo.results import DeleteResult
 
 from autobots.conn.aws.s3 import S3, get_s3
 from autobots.conn.pinecone.pinecone import Pinecone, get_pinecone
 from autobots.datastore.datastore_meta_crud import DatastoreMetaCRUD
 from autobots.datastore.datastore import Datastore
-from autobots.datastore.datastore_meta_orm_model import DatastoreMetaORM
+from autobots.datastore.datastore_meta_doc_model import DatastoreMetaDocCreate, DatastoreMetaDoc, DatastoreMetaDocFind
 from autobots.user.user_orm_model import UserORM
 
-# TODO: make Datastore a Field
+
 class UserDatastore(Datastore):
 
-    def __init__(self, user: UserORM, db: Session, s3: S3 = get_s3(), pinecone: Pinecone = get_pinecone()):
+    def __init__(self, user: UserORM, db: Database, s3: S3 = get_s3(), pinecone: Pinecone = get_pinecone()):
         super().__init__(s3, pinecone)
         self.user = user
         self.db = db
+        self.datastore_meta_crud = DatastoreMetaCRUD(self.db)
 
-    async def init(self, name: str) -> DatastoreMetaORM:
+    async def init(self, name: str) -> DatastoreMetaDoc:
         super().init(name)
-        datastore_meta = DatastoreMetaORM(user_id=self.user.id, name=self.name, id=self.id)
-        user_datastore_meta = await DatastoreMetaCRUD.create(datastore_meta, self.db)
+        datastore_meta_doc_create = DatastoreMetaDocCreate(user_id=str(self.user.id), datastore_id=self.id, name=self.name)
+        user_datastore_meta = await self.datastore_meta_crud.insert_one(datastore_meta_doc_create)
         return user_datastore_meta
 
-    async def hydrate(self, id: str) -> Datastore:
-        datastore_meta = await DatastoreMetaCRUD.read_by_id(self.user.id, id, self.db)
-        datastore = super().hydrate(datastore_meta.id)
+    async def hydrate(self, datastore_id: str) -> Datastore:
+        datastore_meta_doc_find = DatastoreMetaDocFind(user_id=str(self.user.id), datastore_id=datastore_id)
+        datastore_metas = await self.datastore_meta_crud.find(datastore_meta_doc_find)
+        if len(datastore_metas) != 1:
+            raise HTTPException(405, "Datastore not found")
+        datastore_meta = datastore_metas[0]
+        datastore = super().hydrate(datastore_meta.datastore_id)
         return datastore
 
-    async def get(self, name: str) -> DatastoreMetaORM:
-        user_datastore_meta = await DatastoreMetaCRUD.read_by_name_version(
-            self.user.id, name, db=self.db
-        )
-        return user_datastore_meta[0]
+    async def get(self, name: str) -> DatastoreMetaDoc:
+        datastore_meta_doc_find = DatastoreMetaDocFind(user_id=str(self.user.id), name=name)
+        datastore_metas = await self.datastore_meta_crud.find(datastore_meta_doc_find)
+        if len(datastore_metas) != 1:
+            raise HTTPException(405, "Datastore not found")
+        return datastore_metas[0]
 
-    async def list(self, db: Session, limit: int = 100, offset: int = 0) -> List[DatastoreMetaORM]:
-        user_datastore_metas = await DatastoreMetaCRUD.list(
-            self.user.id, limit, offset, db=self.db
-        )
-        return user_datastore_metas
+    async def get_by_datastore_id(self, datastore_id: str) -> DatastoreMetaDoc:
+        datastore_meta_doc_find = DatastoreMetaDocFind(user_id=str(self.user.id), datastore_id=datastore_id)
+        datastore_metas = await self.datastore_meta_crud.find(datastore_meta_doc_find)
+        if len(datastore_metas) != 1:
+            raise HTTPException(405, "Datastore not found")
+        return datastore_metas[0]
 
-    async def delete(self, db: Session, id: str) -> DatastoreMetaORM:
+    async def list(self, limit: int = 100, offset: int = 0) -> List[DatastoreMetaDoc]:
+        datastore_meta_doc_find = DatastoreMetaDocFind(user_id=str(self.user.id))
+        datastore_metas = await self.datastore_meta_crud.find(datastore_meta_doc_find, limit, offset)
+        return datastore_metas
+
+    async def delete(self, datastore_id: str) -> DeleteResult:
         # delete s3 and pinecone entries
-        user_datastore = await self.hydrate(id)
+        user_datastore = await self.hydrate(datastore_id)
         await user_datastore.empty_and_close()
         # delete meta entry
-        user_datastore_meta = await DatastoreMetaCRUD.read_by_id(self.user.id, id, self.db)
-        deleted_meta = await DatastoreMetaCRUD.delete(user_datastore_meta, self.db)
-        return deleted_meta
+        datastore_meta_doc_find = DatastoreMetaDocFind(user_id=str(self.user.id), datastore_id=datastore_id)
+        deleted_result = await self.datastore_meta_crud.delete_many(datastore_meta_doc_find)
+        return deleted_result
