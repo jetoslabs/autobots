b29c5a404827744a88e4629d0ca0107cb2b3310c
Anurag Jha <toanuragjha@gmail.com>
2023-12-09 01:31:29 -0800

    feat-openai-assistant: action market chore

---
+++ autobots/action/action_market/action_market_model.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from typing import Optional, Dict, Any
+
+from pydantic import BaseModel, Field
+
+from autobots.action.action_type.action_types import ActionType
+
+
+class ActionMarketFind(BaseModel):
+    """
+    Input from User to find action
+    """
+    id: Optional[str] = Field(default=None)  # , alias='_id')
+    name: Optional[str] = None
+    version: Optional[float] = None
+    description: Optional[str] = None
+    type: Optional[ActionType] = None
+    created_at: Optional[datetime] = None
+
+
+class ActionMarketDocFind(ActionMarketFind):
+    """
+    Action if published can be fetched, not connected to user_id
+    """
+    is_published: bool = True
