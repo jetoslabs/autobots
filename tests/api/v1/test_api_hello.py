82b6955493045f714d74a2c4a65f6a49da67b1df
Anurag Jha <toanuragjha@gmail.com>
2023-12-09 02:16:19 -0800

    feat-openai-assistant: test chore

--- tests/api/v1/test_api_hello.py
+++ tests/api/v1/test_api_hello.py
@@ -1,8 +1,10 @@
+import pytest
 from fastapi.testclient import TestClient
 
 from conftest import test_client
 
 
+@pytest.mark.asyncio
 def test_hello_happy_path(test_client: TestClient):
     response = test_client.get("/v1/hello")
     assert response.status_code == 200
