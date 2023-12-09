ad1442f2f7f036bad152698fb6262a22e3736074
Anurag Jha <toanuragjha@gmail.com>
2023-12-09 02:04:07 -0800

    feat-openai-assistant: test chore

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,14 @@ from fastapi.testclient import TestClient
 
 from autobots.main import app
 
+
+@pytest_asyncio.fixture
+def set_test_settings():
+    print("Function scope fixture for test: runs for every test")
+
+
 @pytest_asyncio.fixture
 def test_client() -> TestClient:
-    print("Function scope fixture for test: runs for every test")
+    print("Function scope fixture for test: test_client")
     client = TestClient(app)
     return client
