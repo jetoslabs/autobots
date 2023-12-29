1b1d544c0283518679bfea5b602637c749673d2a
Anurag Jha <toanuragjha@gmail.com>
2023-12-29 14:23:02 -0800

    feat-openai-assistant: io_mapper

--- autobots/action/action_type/action_text2text/action_text2text_io_mapper/io_mapper.py
+++ autobots/action/action_type/action_text2text/action_text2text_io_mapper/io_mapper.py
@@ -3,7 +3,7 @@ from typing import Dict, Any
 from pydantic import BaseModel
 
 from autobots.action.action.common_action_models import TextObj, TextObjs
-# from autobots.action.action_type.action_data_type_factory import ActionDataTypeFactory
+from autobots.action.action_type.action_text2text.action_text2text_io_mapper.action_map_copy import ACTION_MAP_COPY
 from autobots.action.action_type.action_text2text.action_text2text_llm_chat_with_vector_search_openai import \
     ActionCreateText2TextLlmChatWithVectorSearchOpenaiConfig, ActionText2TextLlmChatWithVectorSearchOpenai
 from autobots.action.action_type.action_types import ActionType
@@ -11,9 +11,9 @@ from autobots.action.action_type.action_types import ActionType
 
 class IOMapperConfig(BaseModel):
     chat_config: ActionCreateText2TextLlmChatWithVectorSearchOpenaiConfig
-    input_action_output: Dict[str, Any]
-    input_action_type: ActionType | None = None
-    output_action_type: ActionType
+    prev_action_output: Dict[str, Any]
+    prev_action_type: ActionType | None = None
+    next_action_type: ActionType
 
 
 class IOMapper():
@@ -38,19 +38,17 @@ class IOMapper():
         return instruction
 
     async def _build_data_context(self, mapper_config: IOMapperConfig) -> str:
-        context = f"Input data is:\n{mapper_config.input_action_output}"
+        context = f"Input data is:\n{mapper_config.prev_action_output}"
         return context
 
     async def _build_mapping_context(self, mapper_config: IOMapperConfig) -> str:
         context = ""
-        # if mapper_config.input_action:
-        #     input_action_output_data_type = await ActionDataTypeFactory.get_data_types(
-        #         mapper_config.input_action_type, False, False, True
-        #     )
-        #     context = context + f"\nInput datatype is:\n{input_action_output_data_type.output}\n"
-        # if mapper_config.output_action:
-        #     output_action_data_type = await ActionDataTypeFactory.get_data_types(
-        #         mapper_config.output_action_type, False, True, False
-        #     )
-        #     context = context + f"\nOutput datatype is:\n{output_action_data_type.input}"
+        if mapper_config.input_action:
+            # USING ACTION_MAP_COPY TO SAVE FROM CIRCULAR IMPORT
+            input_action_output_data_type = ACTION_MAP_COPY.get(mapper_config.prev_action_type).get_output_type().model_json_schema()
+            context = context + f"\nInput datatype is:\n{input_action_output_data_type}\n"
+        if mapper_config.output_action:
+            # USING ACTION_MAP_COPY TO SAVE FROM CIRCULAR IMPORT
+            output_action_data_type = ACTION_MAP_COPY.get(mapper_config.next_action_type).get_input_type().model_json_schema()
+            context = context + f"\nOutput datatype is:\n{output_action_data_type}"
         return context
