444e358e0d570370f6a1b13a8db6baa4b05d3b0e
Anurag Jha <toanuragjha@gmail.com>
2024-01-26 11:49:39 -0800

    feat-ordered-list: crud find sorted by created_at

--- src/autobots/core/database/mongo_base_crud.py
+++ src/autobots/core/database/mongo_base_crud.py
@@ -40,7 +40,7 @@ class CRUDBase(Generic[DocType, DocCreateType, DocFindType, DocUpdateType]):
         if len(find_params) == 0:
             return []
 
-        cursor = self.document.find(find_params)
+        cursor = self.document.find(find_params).sort("created_at", -1)
         docs = []
 
         skipped = 0
