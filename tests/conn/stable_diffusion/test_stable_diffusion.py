8430288e4dc6da3f804929bd66a5cc401878a421
Anurag Jha <toanuragjha@gmail.com>
2024-01-25 20:53:55 -0800

    feat-stable-diffusion: img2img api and datatypes update

--- tests/conn/stable_diffusion/test_stable_diffusion.py
+++ tests/conn/stable_diffusion/test_stable_diffusion.py
@@ -1,8 +1,8 @@
-
 import pytest
 
 from src.autobots.conn.stable_diffusion.common_models import StableDiffusionRes, YesNo
 from src.autobots.conn.stable_diffusion.image_mixer.image_mixer_model import ImageMixerReqModel
+from src.autobots.conn.stable_diffusion.img2img.img2img_model import SDImg2ImgReqModel
 from src.autobots.conn.stable_diffusion.stable_diffusion import StableDiffusion
 from src.autobots.conn.stable_diffusion.text2img.text2img_model import Text2ImgReqModel
 from src.autobots.conn.stable_diffusion.text2video.text2video_model import Text2VideoReqModel
@@ -30,6 +30,17 @@ async def test_image_mixer_happy_path(set_test_settings):
     )
     st = StableDiffusion(SettingsProvider.sget().STABLE_DIFFUSION_API_KEY)
     res: StableDiffusionRes = await st.image_mixer(req)
+    assert (len(res.urls) >= 1 or res.fetch_url is not None)
+
+
+@pytest.mark.asyncio
+async def test_img2img_happy_path(set_test_settings):
+    prompt = "a cat sitting on a bench"
+    negative_prompt = "bad quality"
+    init_image = "https://raw.githubusercontent.com/CompVis/stable-diffusion/main/data/inpainting_examples/overture-creations-5sI6fQgYIuo.png"
+    req = SDImg2ImgReqModel(prompt=prompt, negetive_prompt=negative_prompt, init_image=init_image, width=512)
+    st = StableDiffusion(SettingsProvider.sget().STABLE_DIFFUSION_API_KEY)
+    res: StableDiffusionRes = await st.img2img(req)
     assert (len(res.urls) >= 1 or res.fetch_url is not None)
 
 
