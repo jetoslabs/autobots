746782521f57178f9c56dade2d1cd2ba0f5ef9a1
Anurag Jha <toanuragjha@gmail.com>
2024-02-01 14:20:26 -0800

    feat-graph: Enum value has to be used for pydantic to work

--- src/autobots/core/config.py
+++ src/autobots/core/config.py
@@ -1,11 +1,8 @@
 from enum import Enum
 from functools import lru_cache
-#
 from pydantic import BaseModel
 
 
-#
-#
 class AppEnv(str, Enum):
     local = "local"
     test = "test"
@@ -25,25 +22,11 @@ class LogLevel(str, Enum):
     critical = "CRITICAL"
 
 
-#
-#
-# class OpenaiEngine(str, Enum):
-#     gpt_3_5 = "gpt-3.5-turbo-16k-0613"
-#     gpt_4_8k = "gpt-4"
-#     gpt_4_8k_0314 = "gpt-4-0314"
-#     gpt_4_8k_0613 = "gpt-4-0613"
-#     gpt_4_32k = "gpt-4-32k"
-#     gpt_4_32k_0314 = "gpt-4-32k-0314"
-#
-#
 class Config(BaseModel):
-    APP_ENV: AppEnv = AppEnv
-    LOG_LEVEL: LogLevel = LogLevel
+    APP_ENV: AppEnv = AppEnv.local
+    LOG_LEVEL: LogLevel = LogLevel.debug
 
 
-#     OPENAI_ENGINE: OpenaiEngine = OpenaiEngine
-#
-#
 @lru_cache
 def get_config():
     return Config()
