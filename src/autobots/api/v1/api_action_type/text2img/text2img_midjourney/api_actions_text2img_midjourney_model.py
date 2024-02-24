a1a10e37459f7b53285c86258dbdf65eb3fe4807
Anurag Jha <toanuragjha@gmail.com>
2024-02-23 22:23:32 -0800

    AUT-7: midjourney api related code in a folder

--- src/autobots/api/v1/api_action_type/text2img/text2img_midjourney/api_actions_text2img_midjourney_model.py
+++ src/autobots/api/v1/api_action_type/text2img/text2img_midjourney/api_actions_text2img_midjourney_model.py
@@ -1,7 +1,6 @@
-from pydantic import BaseModel
 
 from src.autobots.api.v1.api_action.api_action_model import ActionCreateAPIModel
-from src.autobots.conn.useapi.text2img.text2img_model import DiscordJobReqModel, DiscordReqModel
+from src.autobots.conn.useapi.text2img.text2img_model import DiscordReqModel
 
 
 class ActionCreateAPIModelText2ImgMidjourney(ActionCreateAPIModel):
