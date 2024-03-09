a9875f415943ebc6a455e3c8202a9dba96bd8ae8
tomakhil <tomakhil@amazon.com>
2024-03-10 01:08:43 +0530

    Adding action wrapper for bulkedit

---
+++ src/autobots/action/action_type/action_img2img/action_img2img_bulkedit_claid.py
@@ -0,0 +1,49 @@
+from typing import Type, Optional
+from loguru import logger
+from pydantic import BaseModel, Field, ValidationError
+
+from src.autobots.action.action_type.abc.IAction import IAction, ActionConfigType, ActionInputType, ActionOutputType, \
+    ActionConfigUpdateType, ActionConfigCreateType
+from src.autobots.action.action_type.action_types import ActionType
+from src.autobots.conn.claid.claid import get_calid_ai
+from src.autobots.conn.claid.claid_model import ClaidRequestModel, ClaidResponse, ClaidErrorResponse
+
+
+class ActionImg2BulkEditClaid(
+    IAction[ClaidRequestModel, ClaidRequestModel, ClaidRequestModel, ClaidRequestModel, ClaidResponse]
+):
+    type = ActionType.img2img_bulk_edit_claid
+
+    @staticmethod
+    def get_config_create_type() -> Type[ActionConfigCreateType]:
+        return ClaidRequestModel
+
+    @staticmethod
+    def get_config_update_type() -> Type[ActionConfigUpdateType]:
+        return ClaidRequestModel
+
+    @staticmethod
+    def get_config_type() -> Type[ActionConfigType]:
+        return ClaidRequestModel
+
+    @staticmethod
+    def get_input_type() -> Type[ActionInputType]:
+        return ClaidRequestModel
+
+    @staticmethod
+    def get_output_type() -> Type[ActionOutputType]:
+        return ClaidResponse
+
+    def __init__(self, action_config: ClaidRequestModel):
+        super().__init__(action_config)
+
+    async def run_action(self, action_input: ClaidRequestModel) -> ClaidResponse | ClaidErrorResponse:
+        claidai = get_calid_ai()
+        try:
+            res : ClaidResponse = claidai.bulkEdit(action_input)
+        except ValidationError as e:
+            logger.error(str(e))
+        except Exception as e:
+            logger.error(str(e))
+
+        return res
---
+++ src/autobots/api/v1/api_action_type/img2img/api_actions_img2img_claid.py
@@ -0,0 +1,61 @@
+from uuid import UUID
+
+import gotrue
+from fastapi import APIRouter, Depends, HTTPException
+from loguru import logger
+from pymongo.database import Database
+from typing import Optional
+from src.autobots.action.action.action_doc_model import ActionDoc, ActionCreate
+from src.autobots.action.action.user_actions import UserActions
+from src.autobots.action.action_type.action_img2img.action_img2img_bulkedit_claid import ActionImg2BulkEditClaid
+from src.autobots.action.action_type.action_types import ActionType
+from src.autobots.auth.security import get_user_from_access_token
+from src.autobots.conn.claid.claid_model import ClaidRequestModel, ClaidResponse
+from src.autobots.core.database.mongo_base import get_mongo_db
+from src.autobots.user.user_orm_model import UserORM
+
+router = APIRouter()
+class ActionCreateImg2ImgBulkEditClaid(ActionCreate):
+    type: ActionType = ActionType.img2img_bulk_edit_claid
+    config: ClaidRequestModel
+    input: Optional[ClaidRequestModel] = None
+    output: Optional[ClaidResponse] = None
+
+
+
+@router.post("/img2img/bulk_edit/claid")
+async def create_action_bulk_edit_claid(
+        action_create: ActionCreateImg2ImgBulkEditClaid,
+        user_res: gotrue.UserResponse = Depends(get_user_from_access_token),
+        db: Database = Depends(get_mongo_db)
+) -> ActionDoc:
+    try:
+        user_orm = UserORM(id=UUID(user_res.user.id))
+        action_doc = await UserActions(user_orm, db).create_action(
+            ActionCreate(**action_create.model_dump())
+        )
+        return action_doc
+    except Exception as e:
+        logger.error(str(e))
+        raise HTTPException(500)
+
+
+@router.post("/img2img/bulk_edit/claid/{action_id}/run")
+async def run_action_image_mixer_stable_diffusion(
+        action_id: str,
+        action_input: ClaidRequestModel,
+        user_res: gotrue.UserResponse = Depends(get_user_from_access_token),
+        db: Database = Depends(get_mongo_db)
+) -> ClaidResponse:
+    try:
+        user_orm = UserORM(id=UUID(user_res.user.id))
+        action = await UserActions(user=user_orm, db=db).get_action(action_id)
+        bulk_edit_claid = ActionImg2BulkEditClaid(
+            ClaidRequestModel.model_validate(action.model_dump())
+        )
+        resp = await bulk_edit_claid.run_action(action_input)
+        return resp
+    except Exception as e:
+        logger.error(str(e))
+        raise HTTPException(500)
+
