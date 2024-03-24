e9ca17ff9813cb6379a15e4ec6a753ec42fd7e1a
Anurag Jha <toanuragjha@gmail.com>
2024-03-24 01:57:51 -0700

    AUT-77: replace_logging_with_loguru

---
+++ src/autobots/core/logging/loguru_handler.py
@@ -0,0 +1,20 @@
+import logging
+from loguru import logger
+
+
+# Add Loguru handler to root logger
+class LoguruHandler(logging.Handler):
+
+    def __init__(self):
+        super().__init__()
+        # Replace the default Python root logger with Loguru
+        logging.root.handlers = [logging.NullHandler()]
+        logging.root.setLevel(logging.DEBUG)
+        logging.propagate = False  # Disable propagation to prevent duplicate logs
+
+    def emit(self, record):
+        logger.opt(depth=7, exception=record.exc_info).log(record.levelno, record.getMessage())
+
+
+def replace_logging_with_loguru():
+    logging.root.addHandler(LoguruHandler())  # Now the root logger will use Loguru for logging
