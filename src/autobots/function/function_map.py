c5e84365f711568ce2aefa2728a63ac23ccfd7a4
tomakhil <tomakhil@amazon.com>
2024-05-06 10:06:34 +0530

    adding function map

---
+++ src/autobots/function/function_map.py
@@ -0,0 +1,7 @@
+from src.autobots.conn.serp.ads.ads import get_local_ads, get_organic_ads, get_ads
+
+FUNCTION_MAP = {
+    "get_local_ads" : get_local_ads,
+    "get_organic_ads" : get_organic_ads,
+    "get_ads" : get_ads
+}
