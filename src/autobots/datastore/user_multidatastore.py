18a3f6b24e9f844f28806797df17bd1bfa86f8ee
Your Name <you@example.com>
2024-07-16 23:52:21 +0530

    fixes

--- src/autobots/datastore/user_multidatastore.py
+++ src/autobots/datastore/user_multidatastore.py
@@ -30,7 +30,7 @@ from src.autobots.user.user_orm_model import UserORM
 class UserDatastore():
 
     def __init__(self, user: UserORM, db: AsyncIOMotorDatabase, s3: S3 = get_s3()):
-        self.datastore = MultiDataStore(s3, get_unstructured_io())
+        self.datastore = MultiDataStore(s3)
         self.user = user
         self.db = db
         self.datastore_meta_crud = DatastoreMetaCRUD(self.db)
@@ -139,47 +139,37 @@ class UserDatastore():
             ocr_detail: Literal["low", "medium", "high"] = "low",
             webhook: Webhook | None = None
     ) -> DatastoreResultDoc:
-        if data:
-            try:
-                datastore_results_1 = await user_datastore.put_data(data, chunk_token_size=chunk_size)
-                datastore_result_doc.result.status_for = datastore_result_doc.result.status_for + datastore_results_1
-                datastore_result_doc = await user_datastore_result.update_datastore_result(
-                    datastore_result_doc.id, DatastoreResultUpdate(**datastore_result_doc.model_dump())
-                )
-            except Exception as e:
-                logger.error(str(e))
-
-        # Setup PartitionParametersParams for Unstructured_IO to be used in put_files or put_urls
-        hi_res_model_name = "yolox"
-        if ocr_detail == "low": hi_res_model_name = "yolox_quantized" # noqa E701
-        elif ocr_detail == "medium": hi_res_model_name = "yolox" # noqa E701
-        elif ocr_detail == "high": hi_res_model_name = "chipper" # noqa E701
-
-        partition_parameters_params = PartitionParametersParams(
-            combine_under_n_chars=chunk_size,
-            strategy="hi_res",
-            hi_res_model_name=hi_res_model_name,
-            pdf_infer_table_structure=True
-        )
+        # if data:
+        #     try:
+        #         datastore_results_1 = await user_datastore.put_data(data)
+        #         datastore_result_doc.result.status_for = datastore_result_doc.result.status_for + datastore_results_1
+        #         datastore_result_doc = await user_datastore_result.update_datastore_result(
+        #             datastore_result_doc.id, DatastoreResultUpdate(**datastore_result_doc.model_dump())
+        #         )
+        #     except Exception as e:
+        #         logger.error(str(e))
 
         if files:
             try:
-                datastore_results_2 = await user_datastore.put_files(files, partition_parameters_params)
-                datastore_result_doc.result.status_for = datastore_result_doc.result.status_for + datastore_results_2
+                for file in files:
+                    datastore_results_2 = await user_datastore.put_data(file.filename)
+                    datastore_result_doc.result.status_for = datastore_result_doc.result.status_for + datastore_results_2
                 datastore_result_doc = await user_datastore_result.update_datastore_result(
                     datastore_result_doc.id, DatastoreResultUpdate(**datastore_result_doc.model_dump())
                 )
             except Exception as e:
                 logger.error(str(e))
-        if urls:
-            try:
-                datastore_results_3 = await user_datastore.put_urls(urls, partition_parameters_params)
-                datastore_result_doc.result.status_for = datastore_result_doc.result.status_for + datastore_results_3
-                datastore_result_doc = await user_datastore_result.update_datastore_result(
-                    datastore_result_doc.id, DatastoreResultUpdate(**datastore_result_doc.model_dump())
-                )
-            except Exception as e:
-                logger.error(str(e))
+
+        # if urls:
+        #     try:
+        #         for url in urls:
+        #             datastore_results_3 = await user_datastore.put_data(str(url))
+        #             datastore_result_doc.result.status_for = datastore_result_doc.result.status_for + datastore_results_3
+        #         datastore_result_doc = await user_datastore_result.update_datastore_result(
+        #             datastore_result_doc.id, DatastoreResultUpdate(**datastore_result_doc.model_dump())
+        #         )
+        #     except Exception as e:
+        #         logger.error(str(e))
 
         datastore_result_doc.status = EventResultStatus.success
         datastore_result_doc = await user_datastore_result.update_datastore_result(
@@ -192,4 +182,3 @@ class UserDatastore():
 
     async def get_datastore_put_result(self, datastore_put_result_id: str) -> DatastoreResultDoc | None:
         return await self.user_datastore_result.get_datastore_result(datastore_put_result_id)
-
