cdc73b03f01cf9f50cca74dbd8ac8d9160a43c1a
Anurag Jha <toanuragjha@gmail.com>
2024-07-21 23:43:50 -0700

    AUT-108: chore

--- tests/action/test_action_text2text_api.py
+++ tests/action/test_action_text2text_api.py
@@ -43,3 +43,34 @@ async def test_action_text2text_api_2_happy_path(set_test_settings):
     run_output = action.get_output_type().model_validate(run_action_obj.output_dict)
     assert run_output.status_code == 200
     assert run_output.content.__contains__("slip")
+
+
+@pytest.mark.skip("For now")
+@pytest.mark.asyncio
+async def test_action_text2text_api_slack_post_message_happy_path(set_test_settings):
+    action_type = ActionType.text2text_api_call
+    action = ACTION_MAP.get(action_type)
+    action_input_dict = {
+        "method": "POST",
+        "url": "https://slack.com/api/chat.postMessage",
+        "headers": {
+            # "Content-Type": "application/json; charset=utf-8",
+            "Authorization": "Bearer xoxb-abc"
+        },
+        "body": {
+            "channel": "C07BRNL20CA",
+            "text": "Hello world :tada:"
+        },
+    }
+    action.get_input_type().model_validate(action_input_dict)
+    action_doc = ActionDoc(
+        id="id_test_action_text2text_api_slack_post_message_happy_path",
+        user_id="user_test_action_text2text_api_slack_post_message_happy_path",
+        name="name_test_action_text2text_api_slack_post_message_happy_path",
+        type=action_type,
+        config=action_input_dict,
+    )
+    run_action_obj = await ActionFactory.run_action(action_doc, {})
+    run_output = action.get_output_type().model_validate(run_action_obj.output_dict)
+    assert run_output.status_code == 200
+    assert run_output.text.__contains__("\"ok\":true")
