f98bcad38a14a75535bb408d641f78690a986e6a
Khushal <iaskhushal@gmail.com>
2024-07-22 16:38:27 +0530

    linkedin settings fix

--- src/autobots/conn/linkedin_scraper/linkedin_scraper.py
+++ src/autobots/conn/linkedin_scraper/linkedin_scraper.py
@@ -3,11 +3,10 @@ from typing import List, Dict
 from loguru import logger
 import requests
 from src.autobots.core.settings import Settings, SettingsProvider
-settings = SettingsProvider.sget()
 
 scraping_url ="https://api.scrapingdog.com/linkedin"
 
-def get_linkedin_scrape(linkedin_id):
+def get_linkedin_scrape(linkedin_id, settings: Settings =  SettingsProvider.sget()):
     params= {"api_key": settings.SCRAPING_API_KEY,"type":"profile" ,"linkId":linkedin_id}
     response= requests.get(scraping_url, params=params)
     return response.json()
