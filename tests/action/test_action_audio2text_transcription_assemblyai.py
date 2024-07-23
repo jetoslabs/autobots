4c09eccf2727d0414fd3e3694525379a2a411fef
Khushal <iaskhushal@gmail.com>
2024-07-23 14:00:53 +0530

    pydantic update

--- tests/action/test_action_audio2text_transcription_assemblyai.py
+++ tests/action/test_action_audio2text_transcription_assemblyai.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.autobots.action.action_type.action_audio2text.action_audio2text_transcription_assemblyai import AudioRes, \
+from src.autobots.action.action_type.action_audio2text.action_audio2text_transcription_assemblyai import AudioRes, AudioUrl, \
     ActionAudio2TextTranscriptionAssemblyai
 from src.autobots.conn.openai.openai_audio.transcription_model import TranscriptionReq
 import asyncio
@@ -8,6 +8,6 @@ import asyncio
 @pytest.mark.asyncio
 async def test_action_audio2text_transcription_assemblyai_happy_path(set_test_settings):
     config = TranscriptionReq()
-    input = AudioRes(url="https://meetkiwiinc.zendesk.com/attachments/token/DV6HIgMxsVG8rINvioXo6u0ZW/?name=recording+316829.mp3")
+    input = AudioUrl(url="https://meetkiwiinc.zendesk.com/attachments/token/DV6HIgMxsVG8rINvioXo6u0ZW/?name=recording+316829.mp3")
     transcription = await ActionAudio2TextTranscriptionAssemblyai(action_config=config).run_action(input)
-    assert transcription!=""
+    assert transcription.text!=""
--- tests/action/test_action_text2text_linkedin.py
+++ tests/action/test_action_text2text_linkedin.py
@@ -8,4 +8,4 @@ async def test_action_text2text_linkedin_happy_path(set_test_settings):
     config = LinkedInReq(linkedin_id=["sample_linkedin_id"])
     input = LinkedInReq(linkedin_id=["sample_linkedin_id"])
     profile_data = await ActionLinkedInScrape(action_config=config).run_action(input)
-    assert profile_data[0]['success']==True
+    assert profile_data.profile_data[0]['success']==True
