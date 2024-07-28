ac4911e77a9f22d8d35af4f69af42a5b66ff88e4
Anurag Jha <toanuragjha@gmail.com>
2024-07-28 16:17:42 -0700

    AUT-108: middleware adding context

--- src/autobots/data_model/context.py
+++ src/autobots/data_model/context.py
@@ -1,10 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from src.autobots.core.utils import gen_uuid
 
 
 class Context(BaseModel):
-    trace_id: str = str(gen_uuid())
+    # trace_id: str = str(gen_uuid())
+    trace_id: str = Field(default_factory=lambda: str(gen_uuid()))
     user_id: str | None = None
 
     # def to_dict(self) -> Dict[str, Any]:
