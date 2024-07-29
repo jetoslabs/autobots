2f1b06cee7270d1f673c77a1f7413b4f260ac8db
Anurag Jha <toanuragjha@gmail.com>
2024-07-28 17:02:01 -0700

    AUT-108: chore

--- src/autobots/crud/crud_mongo_db/crud_mongo_db.py
+++ src/autobots/crud/crud_mongo_db/crud_mongo_db.py
@@ -26,8 +26,9 @@ class MongoDBCRUD(
 
     @staticmethod
     @lru_cache()
-    async def client(ctx: Context) -> AsyncIOMotorClient:
+    async def client(ctx_json: str) -> AsyncIOMotorClient:
         settings = SettingsProvider.sget()
+        ctx = Context.model_validate_json(ctx_json)
         return await MongoDBCRUD._client(ctx, settings)
 
     @staticmethod
