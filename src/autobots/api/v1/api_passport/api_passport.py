79525d7c6ff2b6062f43e0ffefd33ba53776a89a
Khushal <iaskhushal@gmail.com>
2024-07-31 17:45:22 +0530

    pollgraph api

---
+++ src/autobots/api/v1/api_passport/api_passport.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, Depends, HTTPException
+from src.autobots.passport.passport import Passport
+from src.autobots.passport.passport_doc_model import APIKeyCreate
+from src.autobots.passport.passport_crud import APIKeyCRUD
+
+router = APIRouter(prefix="/api/poll_graph", tags=["poll_graph"])
+
+@router.post("/register", response_model=APIKeyCreate)
+async def register_api_key(
+    api_key_create: APIKeyCreate,
+    passport: Passport = Depends(Passport)
+):
+    return await passport.register_api_key(api_key_create)
