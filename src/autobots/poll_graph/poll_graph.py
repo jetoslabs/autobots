31d14e5dd7831b65978050e51de62cc2dc5447c0
Khushal <iaskhushal@gmail.com>
2024-08-08 15:46:58 +0530

    poll_graph run

--- src/autobots/poll_graph/poll_graph.py
+++ src/autobots/poll_graph/poll_graph.py
@@ -3,6 +3,7 @@ import threading
 import asyncio  # Changed from asyncio to asyncio
 import requests
 from pydantic import BaseModel
+from src.autobots.action.action_type.action_factory import ActionFactory
 
 class PollDoc(BaseModel):
     id: str
@@ -34,11 +35,15 @@ class OutputQueue:
     def is_empty(self):
         return self.queue.empty()
 
+    def pop(self):
+        self.queue.pop()
+
 # Define the WorkflowAgent
 
 # Define the PollingAgent
 class PollingAgent:
     def __init__(self, input_action, output_queue, polling_interval=5):
+        self.input_action = input_action
         self.output_queue = output_queue
         self.polling_interval = polling_interval
         self._stop_event = threading.Event()
@@ -50,7 +55,8 @@ class PollingAgent:
     def _poll(self):
         while not self._stop_event.is_set():
             # run llm action here
-
+            action_result = ActionFactory.run_in_background(self.input_action)
+            self.output_queue.add_message(action_result)
             asyncio.sleep(self.polling_interval)
     
     def stop(self):
@@ -60,7 +66,8 @@ class PollingAgent:
 # Example usage
 
 class PollGraph:
-    def __init__(self, input_action, workflow_agent):
+    def __init__(self, input_action, workflow_agent, pollgraph_result=[]):
+        self.pollgraph_result = pollgraph_result
         self.input_action = input_action
         self.workflow_agent = workflow_agent
         self.output_queue = OutputQueue()
@@ -72,18 +79,19 @@ class PollGraph:
         self.polling_agent.start()
 
         # Simulate a separate thread or process for the WorkflowAgent
-        def process_queue():
+        async def process_queue():
             while True:
                 if not self.output_queue.is_empty():
                     message = self.output_queue.get_message()
-                    self.workflow_agent.run_in_background(message)
+                    workflow_result = await self.workflow_agent.run_in_background(message)
+                    self.output_queue.pop()
+                    self.pollgraph_result.append(workflow_result)
                 else:
                     asyncio.sleep(1)
 
         self.processing_thread = threading.Thread(target=process_queue)
         self.processing_thread.start()
 
-        # Allow the system to run for a while
 
     def stop(self):
         # Stop the polling agent
